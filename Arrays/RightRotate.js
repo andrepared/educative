604b319506bfb61f6e13c7918ecdfc55fe7b1fe8
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-01-06 16:22:36 -0600

    Updating

--- Arrays/RightRotate.js
+++ Arrays/RightRotate.js
@@ -6,3 +6,12 @@ implement a function rightRotate(arr, n) that will rotate the given array by n.
 */
 
 
+// Solution #1 
+function rightRotate(arr, n){ 
+    var rotatedList = [] 
+    for(var item=arr.length-n;item<arr.length;item++) 
+      rotatedList.push(arr[item]) 
+    for(var item=0;item<arr.length-n;item++)
+      rotatedList.push(arr[item]) 
+    return rotatedList 
+}
