20f6c915977d0375a6a4bdafafab3ed2053b3ba3
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-01-12 12:41:21 -0600

    Updated TwoPointers with Just Code Script

--- TwoPointers/main.js
+++ TwoPointers/main.js
@@ -35,3 +35,35 @@ function main() {
     
 }
 main();
+
+/** Just the Code-----------
+function isPalindrome(s) {
+    let left = 0,
+        right = s.length - 1;
+
+    while (left < right) {
+		if (s[left] != s[right]) { 
+			return false;
+		}
+		left++; 
+		right--; 
+    }
+    return true;
+}
+
+function main() {
+    let testCases = ["RACEACAR", "A", "ABCDEFGFEDCBA",
+					"ABC", "ABCBA", "ABBA", "RACEACAR"],
+	i = 1;
+
+    testCases.map((s, index) => {
+        console.log("Test Case #", i);
+        console.log("-".repeat(100));
+		console.log(`\tThe input string is '${s}' and the length of the string is ${s.length}.`);
+		console.log("\n\tIs it a palindrome?.....", isPalindrome(s));
+        console.log("-".repeat(100));
+		i++;
+    });
+}
+
+main(); */
