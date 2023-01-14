6f6650ac542586390c73b203d5ace82c43059ab1
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-01-14 09:21:37 -0600

    CountTheEdges

---
+++ Graphs/CountTheEdges/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const LinkedList = require('./LinkedList.js');
+const Node = require('./Node.js');
+const Graph = require('./Graph.js');
+//class Graph => {int vertices, linkedList[] list}
+//class linkedList => {Node head}
+//class Node => {int data, Node nextElement}
+function numEdges(g) {
+  let sum = 0;
+  //Write code here
+  return sum;
+}
