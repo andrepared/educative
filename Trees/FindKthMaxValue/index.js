7d5977c7acd814d780c63bb015c314b598403488
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-01-15 17:57:55 -0600

    Find the Kth Max

---
+++ Trees/FindKthMaxValue/index.js
@@ -0,0 +1,38 @@
+/**Sorting the tree in order */
+
+"use strict";
+const BinarySearchTree = require('./BinarySearchTree.js');
+const Node = require('./Node.js');
+
+function findKthMax(rootNode,k)
+{
+  var tree = []
+  tree=inOrderTraverse(rootNode, tree);
+  console.log(tree);
+  if(((tree.length)-k) >=0 && k>0)
+  { 
+    return tree[tree.length-k]
+  }
+  return null;
+}
+//Helper recursive function to traverse the tree inorder
+function inOrderTraverse(rootNode,tree)
+{
+  if( rootNode !== null)
+  {
+    tree=inOrderTraverse(rootNode.leftChild,tree)
+    tree.push(rootNode.val)
+    tree=inOrderTraverse(rootNode.rightChild,tree)
+  }
+  
+  return tree;
+}
+    
+
+var BST = new BinarySearchTree(6)
+BST.insertBST(1)
+BST.insertBST(133)
+BST.insertBST(12)
+console.log(findKthMax(BST.root, 3));
+
+
