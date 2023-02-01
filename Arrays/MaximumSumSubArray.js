4c13be75f7b712e69d867ed1f4c51954f29bc200
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-01-31 21:33:53 -0600

    Kandane's algorithm

--- Arrays/MaximumSumSubArray.js
+++ Arrays/MaximumSumSubArray.js
@@ -1,28 +1,51 @@
 /**
 Given an array, find the contigous subarray
+Kandane's Algorithm
  */
-let findMaxSumSubArray = function(array_) {
+// let findMaxSumSubArray = function(array_) {
+//   if (array_.length < 1) {
+//     return 0;
+//   }
+
+//   let currMax = array_[0];
+//   let globalMax = array_[0];
+//   let lengtharray = array_.length;
+//   for (let i = 1; i < lengtharray; i++) {
+//     if (currMax < 0) {
+//       currMax = array_[i];
+//     } else {
+//       currMax += array_[i];
+//     }
+
+//     if (globalMax < currMax) {
+//       globalMax = currMax;
+//     }
+//   }
+//   return globalMax;
+// };
+
+
+// The basic idea of Kadane’s algorithm is to scan the entire array and at each position find the maximum sum of the subarray ending there. This is achieved by keeping a current_max for the current array index and a global_max. The algorithm is as follows:
+
+let findMaxSumSubArray = function (array_) {
   if (array_.length < 1) {
     return 0;
   }
-
+  
   let currMax = array_[0];
   let globalMax = array_[0];
-  let lengtharray = array_.length;
-  for (let i = 1; i < lengtharray; i++) {
+  let lengthArray = array_.length;
+  
+  for (let i = 1; i < lengthArray; i++) {
     if (currMax < 0) {
       currMax = array_[i];
     } else {
-      currMax += array_[i];
+      currMax += array_[i]
     }
-
+    
     if (globalMax < currMax) {
       globalMax = currMax;
     }
   }
   return globalMax;
-};
-
-
-//The basic idea of Kadane’s algorithm is to scan the entire array and at each position find the maximum sum of the subarray ending there. This is achieved by keeping a current_max for the current array index and a global_max. The algorithm is as follows:
-
+}
--- Arrays/RearrangeSortedInMaxMin.js
+++ Arrays/RearrangeSortedInMaxMin.js
@@ -1,7 +1,7 @@
 /** Arrange elements in such a way that the maximum element appears at first, then minimum at second, then second maximum at third, and so on and so forth
 
 Problem Statement
-Implement a function called MaxMin(arr), which will rearranvge the elements of a sorted array so that the first position will have the largest number, second will have the smallest, third will have the second largest, and so on and so forth. */
+Implement a function called MaxMin(arr), which will rearrange the elements of a sorted array so that the first position will have the largest number, second will have the smallest, third will have the second largest, and so on and so forth. */
 
 function MaxMin(arr) {
     var result = [];
@@ -16,8 +16,19 @@ function MaxMin(arr) {
         
 }
 
+function MaxMin(arr) {
+  var result = [];
+  for (var i = 0; i < (Math.floor(arr.length / 2)); i++){
+    result.push(arr[arr.length - (i+1)])
+    result.push(arr[i])
+  }
+  if (arr.length % 2) {
+    result.push(arr[Math.floor(arr.length / 2)])
+  }
+  return result;
+}
 // This solution, we first create a new empty array. We then iterate throught the array starting from the Oth index until the arr.length/2th index. So if the length of the given array is 10, the iterator variable i on line 8 in our solution would start from 0 and end at 4. At each iteration, we first append the largest unappended element and then the smallest.
-// So at the first iteration, i = 0, therefore, bu replacing i with 0 in arr[arr.length - (i+1)] we get arr[arr.length-(0+1)] = arr[arr.length-2]. This is equivalent to the lst element of the array, and as the array is sorted, the largest element in the array is appended to the result array first and then the 0th element. NExt, the second largest and the second smallest are appended and so on until the end.
+// So at the first iteration, i = 0, therefore, by replacing i with 0 in arr[arr.length - (i+1)] we get arr[arr.length-(0+1)] = arr[arr.length-2]. This is equivalent to the lst element of the array, and as the array is sorted, the largest element in the array is appended to the result array first and then the 0th element. NExt, the second largest and the second smallest are appended and so on until the end.
 
 // The Time Complexity 
 // 
