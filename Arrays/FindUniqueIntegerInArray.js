c5925a209c2349ad627f9c6a6acb3c588fa971f6
Andres Garcia Paredes <50889119+andrepared@users.noreply.github.com>
2023-01-31 19:13:20 -0600

    reworking files

--- Arrays/FindUniqueIntegerInArray.js
+++ Arrays/FindUniqueIntegerInArray.js
@@ -34,3 +34,20 @@ Time Complexity
 The time complexity of this solution is O(n^2)
  since the entire array is iterated for each element
 →n×n */
+
+function findFirstUnique(arr) {
+
+    var index2;
+    for (var index1 = 0; index1 < arr.length; index1++){
+        index2 = 0
+        while (index2 < arr.length) {
+            if ((index1 != index2) && (arr[index1] == arr[index2]))
+                break
+            index2 += 1
+        }
+        if (index2 == arr.length) {
+            return arr[index1]
+        }
+        return null;
+    }
+}
